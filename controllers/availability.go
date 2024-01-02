da3335740e2cd80f9d60335dfc5dbe4a547b8f6e
Lucas Holter <holter@chalmers.se>
2024-01-02 15:02:05 +0100

    #5 (Ongoing) Add ping/echo functionallity for appointment service

--- controllers/availability.go
+++ controllers/availability.go
@@ -13,16 +13,19 @@ import (
 )
 
 var(
+    AppointmentFlag = make(chan bool, 1)
+    appointmentResponse = make(chan struct{}, 1)
+    appointmentDown bool
+
     UserFlag = make(chan bool, 1)
     userResponse = make(chan struct{}, 1)
-    recievedMessage string
     userDown bool
-    appointmentDown bool
 )
 
 func InitialiseAvailability(client mqtt.Client) {
     checkServiceStatus()
     go CheckUserService(client)
+    go CheckAppointmentService(client)
 
     tokenUserService := client.Subscribe("grp20/res/patients/get", byte(0), func(c mqtt.Client, m mqtt.Message) {
         userResponse <- struct{}{}
@@ -30,6 +33,14 @@ func InitialiseAvailability(client mqtt.Client) {
     if tokenUserService.Error() != nil{
         panic(tokenUserService.Error())
     }
+
+    tokenAppointmentService := client.Subscribe("grp20/res/timeslots/get", byte(0), func(c mqtt.Client, m mqtt.Message) {
+    appointmentResponse <- struct{}{}
+    })
+    if tokenAppointmentService.Error() != nil{
+        panic(tokenAppointmentService.Error())
+    }
+
 }
 
 func CheckUserService(client mqtt.Client) {
@@ -61,6 +72,36 @@ func CheckUserService(client mqtt.Client) {
     }
 }
 
+func CheckAppointmentService(client mqtt.Client) {
+    for{
+        requestID := primitive.NewObjectID().Hex()
+        message := `{"requestID": "` + requestID + `"}`
+        token := client.Publish("grp20/req/timeslots/get", 0, false, message)
+        token.Wait()
+
+        timeout := time.After(5*time.Second)
+        select{
+        case <- appointmentResponse:
+            AppointmentFlag <- true
+            closeDownTime(primitive.NewDateTimeFromTime(time.Now()), "Appointment")
+            appointmentDown = false
+            time.Sleep(5*time.Second)
+        case <- timeout:
+            AppointmentFlag <- false
+            if !appointmentDown{
+                downTime := schemas.DownTime{
+                    TimeDown: primitive.NewDateTimeFromTime(time.Now()),
+                    Service: "Appointment",
+                }
+                insertDownTimeDB(downTime)
+                appointmentDown = true
+            }
+
+        }
+    }
+}
+
+
 func insertDownTimeDB(downTime schemas.DownTime) {
     col := database.Database.Collection("DownTime")
 
@@ -69,27 +110,49 @@ func insertDownTimeDB(downTime schemas.DownTime) {
 }
 
 func checkServiceStatus() {
-    var downTimes []bson.M
+    var userDownTimes []bson.M
     col := database.Database.Collection("DownTime")
     zeroTime := time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
     zeroTimePrim := primitive.NewDateTimeFromTime(zeroTime)
 
+    // User service 
     filter := bson.M{"time_up": zeroTimePrim, "service": "User"}
     res, err := col.Find(context.TODO(), filter)
     if err != nil{
         fmt.Println("Error getting from database")
         return
     }
-    if err := res.All(context.Background(), &downTimes); err != nil{
+    if err := res.All(context.Background(), &userDownTimes); err != nil{
         fmt.Println("Error decoding")
         return
     }
 
-    if len(downTimes) == 1{
+    if len(userDownTimes) == 1{
         userDown = true
         fmt.Println("Service down")
-    } else if len(downTimes) == 0 {
+    } else if len(userDownTimes) == 0 {
         userDown = false
+        fmt.Println("Service not down")
+    }
+
+    // Appointment service
+    var appointDownTimes []bson.M
+    filter = bson.M{"time_up": zeroTimePrim, "service": "Appointment"}
+    res, err = col.Find(context.TODO(), filter)
+    if err != nil{
+        fmt.Println("Error getting from database")
+        return
+    }
+    if err = res.All(context.Background(), &appointDownTimes); err != nil{
+        fmt.Println("Error decoding")
+        return
+    }
+
+    if len(appointDownTimes) == 1{
+        appointmentDown = true
+        fmt.Println("Service down")
+    } else if len(appointDownTimes) == 0 {
+        appointmentDown = false
         fmt.Println("Service not down")
     }
 }
