3dc9d1e9bd29bdb6bd373f5852b0585b7dfc7636
Cornelia Olofsson <guscornol@student.gu.se>
2024-01-08 20:10:07 +0100

    #6 (ongoing) remove unused methods and add conditional filtering for CalculatePercentage

--- controllers/monitoring/logRatio.go
+++ controllers/monitoring/logRatio.go
@@ -8,50 +8,25 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-// This controller uses no time filter, only an empty Bson.D document. This can and will be changed
-
-// Returns the number of requests with no filter
-func GetNumOfRequests() (int32, error) {
-	requests := database.GetCollection("RequestLogs")
-
-	// Count all documents
-	count, err := requests.CountDocuments(context.TODO(), bson.D{})
-	if err != nil {
-		return 0, err
-	}
-
-	// Convert int64 to int32
-	numOfRequests := int32(count)
-	return numOfRequests, nil
-}
-
-// Returns the number of requests with no filter
-func GetNumOfResponses() (int32, error) {
-	requests := database.GetCollection("ResponseLogs")
-
-	// Count all documents
-	count, err := requests.CountDocuments(context.TODO(), bson.D{})
-	if err != nil {
-		return 0, err
-	}
-
-	// Convert int64 to int32
-	numOfResponses := int32(count)
-	return numOfResponses, nil
-}
-
-// Calculates the percentage of responses relative to requests for a specific service
+// CalculatePercentage Calculates the percentage of responses relative to requests for a specific service
 func CalculatePercentage(service string) (float64, error) {
 	requests := database.GetCollection("RequestLogs")
 	responses := database.GetCollection("ResponseLogs")
 
-	// Count requests with the specified service
-	countRequests, err := requests.CountDocuments(context.TODO(), bson.D{{"service", service}})
+	var filter bson.D
+	if service != "AllServices" {
+		filter = bson.D{{"service", service}}
+	} else {
+		filter = bson.D{{}}
+	}
+
+	// Count requests based on the filter
+	countRequests, err := requests.CountDocuments(context.TODO(), filter)
 	if err != nil {
 		return 0, err
 	}
 
-	// Count all responses
+	// Count all responses based on the filter
 	countResponses, err := responses.CountDocuments(context.TODO(), bson.D{})
 	if err != nil {
 		return 0, err
